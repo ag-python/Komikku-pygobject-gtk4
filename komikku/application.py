ag-python/Komikku-pygobject-gtk4

commit c58ee5eaf2df7e7ad887d30f8bfd314f608f3036

[Features] Add Reading History

komikku/application.py
@@ -27,6 +27,7 @@ from komikku.categories_editor import CategoriesEditor
 from komikku.downloader import Downloader
 from komikku.downloader import DownloadManager
 from komikku.explorer import Explorer
+from komikku.history import History
 from komikku.library import Library
 from komikku.models import backup_db
 from komikku.models import Settings
@@ -215,6 +216,8 @@ class ApplicationWindow(Handy.ApplicationWindow):
     explorer_search_page_server_website_button = Gtk.Template.Child('explorer_search_page_server_website_button')
     explorer_card_page_add_read_button = Gtk.Template.Child('explorer_card_page_add_read_button')
 
+    history_search_button = Gtk.Template.Child('history_search_button')
+
     preferences_subtitle_label = Gtk.Template.Child('preferences_subtitle_label')
 
     notification_label = Gtk.Template.Child('notification_label')
@@ -314,6 +317,7 @@ class ApplicationWindow(Handy.ApplicationWindow):
         self.categories_editor = CategoriesEditor(self)
         self.download_manager = DownloadManager(self)
         self.explorer = Explorer(self)
+        self.history = History(self)
         self.preferences = Preferences(self)
 
         # Window
@@ -468,11 +472,15 @@ class ApplicationWindow(Handy.ApplicationWindow):
         """
         Go back navigation with <Escape> key:
         - Library <- Manga <- Reader
+        - Library <- History <- Reader
+        - Library <- History <- Card <- Reader
+
         - Explorer: Library <- Servers <- Search <- Card
         - Preferences: Library <- Page <- Subpage
         - Categories Editor: Library <-
+
         - Exit selection mode: Library, Card chapters, Download Manager
-        - Exit search mode: Library, Explorer 'servers' and 'search' pages
+        - Exit search mode: Library, Explorer 'servers' and 'search' pages, History
         """
         if event.keyval == Gdk.KEY_Escape:
             self.on_left_button_clicked()
@@ -501,16 +509,23 @@ class ApplicationWindow(Handy.ApplicationWindow):
                 self.card.leave_selection_mode()
             else:
                 self.card.stop_populate()
-                self.library.show(invalidate_sort=True)
+
+                if self.card.came_from == 'library':
+                    self.library.show(invalidate_sort=True)
+                elif self.card.came_from == 'history':
+                    self.history.show()
 
         elif self.page == 'reader':
             self.reader.remove_pager()
             self.set_unfullscreen()
 
-            # Refresh to update all previously chapters consulted (last page read may have changed)
-            # and update info like disk usage
-            self.card.refresh(self.reader.chapters_consulted)
-            self.card.show()
+            if self.reader.came_from == 'card':
+                # Refresh to update all previously chapters consulted (last page read may have changed)
+                # and update info like disk usage
+                self.card.refresh(self.reader.chapters_consulted)
+                self.card.show()
+            elif self.reader.came_from == 'history':
+                self.history.show()
 
         elif self.page == 'categories_editor':
             self.library.show()
@@ -523,6 +538,9 @@ class ApplicationWindow(Handy.ApplicationWindow):
 
         elif self.page == 'explorer':
             self.explorer.navigate_back(source)
+
+        elif self.page == 'history':
+            self.history.navigate_back(source)
 
         elif self.page == 'preferences':
             self.preferences.navigate_back(source)
@@ -610,10 +628,10 @@ class ApplicationWindow(Handy.ApplicationWindow):
 
         if not transition:
             transition_type = Gtk.StackTransitionType.NONE
-        elif name in ('categories_editor', 'download_manager', 'explorer', 'preferences'):
+        elif name in ('categories_editor', 'download_manager', 'explorer', 'history', 'preferences'):
             transition_type = Gtk.StackTransitionType.SLIDE_RIGHT
         else:
-            if self.page in ('categories_editor', 'download_manager', 'explorer', 'preferences'):
+            if self.page in ('categories_editor', 'download_manager', 'explorer', 'history', 'preferences'):
                 transition_type = Gtk.StackTransitionType.SLIDE_LEFT
             else:
                 transition_type = self.stack.get_transition_type()

data/info.febvre.Komikku.gresource.xml
@@ -8,6 +8,7 @@
         <file compressed="true" preprocess="xml-stripblanks">ui/categories_editor.ui</file>
         <file compressed="true" preprocess="xml-stripblanks">ui/download_manager.ui</file>
         <file compressed="true" preprocess="xml-stripblanks">ui/explorer.ui</file>
+        <file compressed="true" preprocess="xml-stripblanks">ui/history.ui</file>
         <file compressed="true" preprocess="xml-stripblanks">ui/preferences.ui</file>
         <file compressed="true" preprocess="xml-stripblanks">ui/shortcuts_overview.ui</file>
 

data/ui/application_window.ui
@@ -248,6 +248,20 @@
                         <property name="position">6</property>
                       </packing>
                     </child>
+                    <child>
+                      <object class="GtkLabel" id="history_title_label">
+                        <property name="visible">True</property>
+                        <property name="can-focus">False</property>
+                        <property name="label" translatable="yes">History</property>
+                        <style>
+                          <class name="title"/>
+                        </style>
+                      </object>
+                      <packing>
+                        <property name="name">history</property>
+                        <property name="position">7</property>
+                      </packing>
+                    </child>
                   </object>
                 </child>
                 <child>
@@ -440,6 +454,25 @@
                       <packing>
                         <property name="name">explorer.servers</property>
                         <property name="position">6</property>
+                      </packing>
+                    </child>
+                    <child>
+                      <object class="GtkToggleButton" id="history_search_button">
+                        <property name="visible">True</property>
+                        <property name="can-focus">True</property>
+                        <property name="receives-default">True</property>
+                        <property name="tooltip-text" translatable="yes">Search</property>
+                        <child>
+                          <object class="GtkImage">
+                            <property name="visible">True</property>
+                            <property name="can-focus">False</property>
+                            <property name="icon-name">edit-find-symbolic</property>
+                          </object>
+                        </child>
+                      </object>
+                      <packing>
+                        <property name="name">history</property>
+                        <property name="position">7</property>
                       </packing>
                     </child>
                   </object>

data/ui/history.ui
@@ -1,0 +1,108 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- Generated with glade 3.38.2 -->
+<interface>
+  <requires lib="gtk+" version="3.20"/>
+  <requires lib="libhandy" version="1.2"/>
+  <template class="History" parent="GtkBox">
+    <property name="visible">True</property>
+    <property name="can-focus">False</property>
+    <property name="orientation">vertical</property>
+    <child>
+      <object class="HdySearchBar" id="searchbar">
+        <property name="visible">True</property>
+        <property name="can-focus">False</property>
+        <child>
+          <object class="HdyClamp">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="margin-left">6</property>
+            <property name="margin-right">6</property>
+            <property name="margin-start">6</property>
+            <property name="margin-end">6</property>
+            <property name="maximum-size">800</property>
+            <child>
+              <object class="GtkSearchEntry" id="searchentry">
+                <property name="visible">True</property>
+                <property name="can-focus">True</property>
+                <property name="hexpand">True</property>
+                <property name="primary-icon-name">edit-find-symbolic</property>
+                <property name="primary-icon-activatable">False</property>
+                <property name="primary-icon-sensitive">False</property>
+              </object>
+            </child>
+          </object>
+        </child>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">0</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkScrolledWindow">
+        <property name="visible">True</property>
+        <property name="can-focus">True</property>
+        <child>
+          <object class="GtkViewport">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="shadow-type">none</property>
+            <child>
+              <object class="HdyClamp">
+                <property name="visible">True</property>
+                <property name="can-focus">False</property>
+                <property name="margin-start">12</property>
+                <property name="margin-end">12</property>
+                <property name="margin-top">12</property>
+                <property name="margin-bottom">12</property>
+                <property name="maximum-size">800</property>
+                <child>
+                  <object class="GtkStack" id="stack">
+                    <property name="visible">True</property>
+                    <property name="can-focus">False</property>
+                    <child>
+                      <placeholder/>
+                    </child>
+                    <child>
+                      <object class="GtkBox" id="dates_box">
+                        <property name="visible">True</property>
+                        <property name="can-focus">False</property>
+                        <property name="orientation">vertical</property>
+                        <property name="spacing">6</property>
+                        <child>
+                          <placeholder/>
+                        </child>
+                      </object>
+                      <packing>
+                        <property name="name">list</property>
+                        <property name="position">1</property>
+                      </packing>
+                    </child>
+                    <child>
+                      <object class="HdyStatusPage" id="empty_status_page">
+                        <property name="visible">True</property>
+                        <property name="can-focus">False</property>
+                        <property name="icon-name">document-open-recent-symbolic</property>
+                        <property name="title" translatable="yes">No history</property>
+                      </object>
+                      <packing>
+                        <property name="name">empty</property>
+                        <property name="position">2</property>
+                      </packing>
+                    </child>
+                  </object>
+                </child>
+              </object>
+            </child>
+          </object>
+        </child>
+      </object>
+      <packing>
+        <property name="expand">True</property>
+        <property name="fill">True</property>
+        <property name="position">1</property>
+      </packing>
+    </child>
+  </template>
+</interface>

data/ui/menu/main.xml
@@ -15,6 +15,10 @@
                 <attribute name="label" translatable="yes">Categories</attribute>
                 <attribute name="action">app.library.categories-editor</attribute>
             </item>
+            <item>
+                <attribute name="label" translatable="yes">History</attribute>
+                <attribute name="action">app.library.history</attribute>
+            </item>
         </section>
         <section>
             <item>

komikku/card.py
@@ -30,6 +30,7 @@ from komikku.utils import scale_pixbuf_animation
 
 
 class Card:
+    came_from = None
     manga = None
     selection_mode = False
 
@@ -81,6 +82,8 @@ class Card:
         self.window.menu_button.set_menu_model(self.builder.get_object('menu-card-selection-mode'))
 
     def init(self, manga, transition=True):
+        self.came_from = self.window.page
+
         # Default page is `Info` page except when we come from Explorer
         self.stack.set_visible_child_name('chapters' if self.window.page == 'explorer' else 'info')
 

komikku/history.py
@@ -1,0 +1,214 @@
+# Copyright (C) 2019-2021 Valéry Febvre
+# SPDX-License-Identifier: GPL-3.0-only or GPL-3.0-or-later
+# Author: Valéry Febvre <vfebvre@easter-eggs.com>
+
+import datetime
+from gettext import gettext as _
+import pytz
+
+from gi.repository import Gdk
+from gi.repository import GLib
+from gi.repository import GObject
+from gi.repository import Gtk
+from gi.repository import Handy
+from gi.repository.GdkPixbuf import Pixbuf
+from gi.repository.GdkPixbuf import PixbufAnimation
+
+from komikku.models import Chapter
+from komikku.models import create_db_connection
+from komikku.servers.utils import get_file_mime_type
+from komikku.utils import scale_pixbuf_animation
+
+THUMB_WIDTH = 45
+THUMB_HEIGHT = 62
+DAYS_LIMIT = 30
+
+
+@Gtk.Template.from_resource('/info/febvre/Komikku/ui/history.ui')
+class History(Gtk.Box):
+    __gtype_name__ = 'History'
+
+    stack = Gtk.Template.Child('stack')
+    dates_box = Gtk.Template.Child('dates_box')
+    searchbar = Gtk.Template.Child('searchbar')
+    searchentry = Gtk.Template.Child('searchentry')
+
+    def __init__(self, window):
+        Gtk.ScrolledWindow.__init__(self)
+
+        self.window = window
+        self.builder = window.builder
+
+        self.search_button = self.window.history_search_button
+        self.searchbar.connect_entry(self.searchentry)
+        self.searchbar.bind_property(
+            'search-mode-enabled', self.search_button, 'active', GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE
+        )
+        self.searchentry.connect('activate', self.on_searchentry_activated)
+        self.searchentry.connect('changed', self.search)
+
+        self.search_button.connect('clicked', self.toggle_search)
+
+        self.window.connect('key-press-event', self.on_key_press)
+        self.window.stack.add_named(self, 'history')
+
+    def filter(self, row):
+        """
+        This function gets one row and has to return:
+        - True if the row should be displayed
+        - False if the row should not be displayed
+        """
+        term = self.searchentry.get_text().strip().lower()
+
+        ret = (
+            term in row.chapter.title.lower() or
+            term in row.chapter.manga.name.lower()
+        )
+
+        if ret:
+            # As soon as a row is visible, made grand parent date_box visible
+            GLib.idle_add(row.get_parent().get_parent().show)
+
+        return ret
+
+    def navigate_back(self, source):
+        # Back to Library if:
+        # - user click on 'Back' button
+        # - or use 'Esc' key and not in search mode
+        if source == 'click' or not self.searchbar.get_search_mode():
+            self.window.library.show()
+
+        # Leave search mode
+        if self.searchbar.get_search_mode():
+            self.searchbar.set_search_mode(False)
+
+    def on_key_press(self, _widget, event):
+        """Search entry can be focused by simply typing a printable character"""
+
+        if self.window.page != 'history':
+            return Gdk.EVENT_PROPAGATE
+
+        return self.searchbar.handle_event(event)
+
+    def on_searchentry_activated(self, _entry):
+        if not self.searchbar.get_search_mode():
+            return
+
+        row = self.dates_box.get_children()[0].get_children()[-1].get_row_at_y(0)
+        if row:
+            self.window.reader.init(row.chapter.manga, row.chapter)
+
+    def populate(self):
+        for box in self.dates_box:
+            box.destroy()
+
+        db_conn = create_db_connection()
+        start = (datetime.date.today() - datetime.timedelta(days=DAYS_LIMIT)).strftime('%Y-%m-%d')
+        records = db_conn.execute('SELECT * FROM chapters WHERE last_read >= ? ORDER BY last_read DESC', (start,)).fetchall()
+        db_conn.close()
+
+        if records:
+            local_timezone = datetime.datetime.utcnow().astimezone().tzinfo
+            today = datetime.date.today()
+            yesterday = today - datetime.timedelta(days=1)
+
+            date = None
+            for record in records:
+                chapter = Chapter.get(record['id'])
+
+                # Create new Box (Label + ListBox) when date change
+                if date is None or date != chapter.last_read.date():
+                    box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+
+                    date = chapter.last_read.date()
+                    if date == today:
+                        label = _('Today')
+                    elif date == yesterday:
+                        label = _('Yesterday')
+                    else:
+                        label = date.strftime(_('%Y-%m-%d'))
+                    label = Gtk.Label(label=label, xalign=0, margin=6)
+                    label.get_style_context().add_class('title-4')
+                    label.get_style_context().add_class('dim-label')
+                    box.add(label)
+
+                    listbox = Gtk.ListBox()
+                    listbox.get_style_context().add_class('content')
+                    listbox.set_filter_func(self.filter)
+                    box.add(listbox)
+
+                    self.dates_box.add(box)
+
+                action_row = Handy.ActionRow(activatable=True, selectable=False)
+                action_row.connect('activated', self.on_row_activated)
+                action_row.chapter = chapter
+
+                action_row.set_title(chapter.manga.name)
+                action_row.set_subtitle(chapter.title)
+
+                # Cover
+                if chapter.manga.cover_fs_path is None:
+                    pixbuf = Pixbuf.new_from_resource_at_scale(
+                        '/info/febvre/Komikku/images/missing_file.png', THUMB_WIDTH, THUMB_HEIGHT, False)
+                else:
+                    try:
+                        if get_file_mime_type(chapter.manga.cover_fs_path) != 'image/gif':
+                            pixbuf = Pixbuf.new_from_file_at_scale(chapter.manga.cover_fs_path, THUMB_WIDTH, THUMB_HEIGHT, False)
+                        else:
+                            animation_pixbuf = scale_pixbuf_animation(
+                                PixbufAnimation.new_from_file(chapter.manga.cover_fs_path), THUMB_WIDTH, THUMB_HEIGHT, False)
+                            pixbuf = animation_pixbuf.get_static_image()
+                    except Exception:
+                        # Invalid image, corrupted image, unsupported image format,...
+                        pixbuf = Pixbuf.new_from_resource_at_scale(
+                            '/info/febvre/Komikku/images/missing_file.png', THUMB_WIDTH, THUMB_HEIGHT, False)
+
+                action_row.add_prefix(Gtk.Image.new_from_pixbuf(pixbuf))
+
+                # Time
+                last_read = chapter.last_read.replace(tzinfo=pytz.UTC).astimezone(local_timezone)
+                label = Gtk.Label(label=last_read.strftime('%H:%M'))
+                label.get_style_context().add_class('subtitle')
+                action_row.add(label)
+
+                # Play button
+                button = Gtk.Button.new_from_icon_name('media-playback-start-symbolic', Gtk.IconSize.BUTTON)
+                button.connect('clicked', self.on_row_play_button_clicked, action_row)
+                button.set_valign(Gtk.Align.CENTER)
+                action_row.add(button)
+
+                listbox.add(action_row)
+
+            self.dates_box.show_all()
+            self.stack.set_visible_child_name('list')
+        else:
+            self.stack.set_visible_child_name('empty')
+
+    def on_row_activated(self, row):
+        self.window.card.init(row.chapter.manga)
+
+    def on_row_play_button_clicked(self, _button, row):
+        self.window.reader.init(row.chapter.manga, row.chapter)
+
+    def search(self, _entry):
+        for date_box in self.dates_box.get_children():
+            listbox = date_box.get_children()[1]
+            listbox.invalidate_filter()
+            # Hide date_box, will be shown if a least one row of listbox is not filtered
+            date_box.hide()
+
+    def show(self, transition=True):
+        self.populate()
+
+        self.window.left_button.set_tooltip_text(_('Back'))
+        self.window.left_button_image.set_from_icon_name('go-previous-symbolic', Gtk.IconSize.BUTTON)
+        self.window.library_flap_reveal_button.hide()
+
+        self.window.right_button_stack.set_visible_child_name('history')
+
+        self.window.menu_button.hide()
+
+        self.window.show_page('history', transition=transition)
+
+    def toggle_search(self, button):
+        self.searchbar.set_search_mode(button.get_active())

komikku/library.py
@@ -155,6 +155,10 @@ class Library:
         categories_editor_action.connect('activate', self.open_categories_editor)
         self.window.application.add_action(categories_editor_action)
 
+        history_action = Gio.SimpleAction.new('library.history', None)
+        history_action.connect('activate', self.open_history)
+        self.window.application.add_action(history_action)
+
         # Search menu actions
         search_downloaded_action = Gio.SimpleAction.new_stateful('library.search.downloaded', None, GLib.Variant('b', False))
         search_downloaded_action.connect('change-state', self.on_search_menu_action_changed)
@@ -443,6 +447,9 @@ class Library:
 
     def open_download_manager(self, action, param):
         self.window.download_manager.show()
+
+    def open_history(self, action, param):
+        self.window.history.show()
 
     def populate(self):
         db_conn = create_db_connection()

commit 825b5382c782312df1e54e6c14aaf9fd6d5f5329

Fix regression introduced in c58ee5eaf
Fix #169

komikku/application.py
@@ -510,7 +510,7 @@ class ApplicationWindow(Handy.ApplicationWindow):
             else:
                 self.card.stop_populate()
 
-                if self.card.came_from == 'library':
+                if self.card.came_from in ('library', 'explorer'):
                     self.library.show(invalidate_sort=True)
                 elif self.card.came_from == 'history':
                     self.history.show()