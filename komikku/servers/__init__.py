2b3a06489a23adbe3cd4f29c88c94a9fdc779b4e
Valéry Febvre <vfebvre@easter-eggs.com>
2022-08-25 00:23:43 +0200

    [Servers] Headless brwoser: Slightly adjusted webview settings

--- komikku/servers/__init__.py
+++ komikku/servers/__init__.py
@@ -103,8 +103,9 @@ class HeadlessBrowser(Gtk.Window):
         self.viewport.add(self.webview)
 
         self.settings = self.webview.get_settings()
-        self.settings.set_enable_dns_prefetching(True)
         self.settings.set_enable_page_cache(False)
+        self.settings.set_enable_frame_flattening(True)
+        self.settings.set_enable_accelerated_2d_canvas(True)
         # self.settings.props.enable_developer_extras = True
 
         self.web_context = self.webview.get_context()
